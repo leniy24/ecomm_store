520013597f6f71cc83a92e545a345b1bfb16862f
leniy24 <xktjxj@gmail.com>
2025-06-02 00:57:23 +0530

    Fix Firebase configuration and dependencies

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,11 +1,9 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAnalytics } from "firebase/analytics";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDVCEWUMiSPmjmlSQDHakagoHyZlLlLncs",
   authDomain: "e-comm-814bf.firebaseapp.com",
@@ -16,12 +14,10 @@ const firebaseConfig = {
   measurementId: "G-TST7W89JLR"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
 const analytics = getAnalytics(app);
 
-// Export the Firebase services
 export { app, auth, db, storage, analytics };
