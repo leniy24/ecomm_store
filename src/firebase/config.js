47a2419b75ce6ecefc6ec165a7fdd1e2b8005439
leniy24 <xktjxj@gmail.com>
2025-06-02 00:59:14 +0530

    Fix Firebase setup and dependencies

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDVCEWUMiSPmjmlSQDHakagoHyZlLlLncs",
   authDomain: "e-comm-814bf.firebaseapp.com",
   projectId: "e-comm-814bf",
-  storageBucket: "e-comm-814bf.appspot.com",
+  storageBucket: "e-comm-814bf.firebasestorage.app",
   messagingSenderId: "389880259864",
   appId: "1:389880259864:web:6ed43dd1a07c0e4aab6a6d",
   measurementId: "G-TST7W89JLR"
